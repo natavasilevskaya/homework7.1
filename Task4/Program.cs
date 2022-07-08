71773ded3c446513b687da8e7a027327e70a38bf
Nata.Vasilevskaya <_nata.vasilevskaya11@yandex.ru>
2022-07-08 20:01:41 +0300

    Task4 file added

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// программа принимает на вход число N, а на выходе показывает все четные числа от 1 до N
+
+int num;
+
+Console.Write("Input your number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i<=num; i++) // если 0 считается 
+{
+    if(i%2 == 0) 
+    {
+        Console.Write("{0} ", i);
+    }
+}
