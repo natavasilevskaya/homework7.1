8021136e514a158877d9e49c6b01f7d9f7a51365
Nata.Vasilevskaya <_nata.vasilevskaya11@yandex.ru>
2022-07-08 19:45:14 +0300

    Task2 file added

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int n1, n2, n3;
+
+Console.WriteLine("Imput number1: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Imput number2: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Imput number3: ");
+n3 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+
+if (n2 > max) max = n2;
+if (n3 > max) max = n3;
+Console.Write("Max number is " + max);
