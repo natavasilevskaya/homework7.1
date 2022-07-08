25349451c037d5f97e84f7e2973d74e04904bd5d
Nata.Vasilevskaya <_nata.vasilevskaya11@yandex.ru>
2022-07-08 19:50:25 +0300

    Task3 file added

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// программа на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int number;
+
+Console.WriteLine("Input number: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+if (number%2 == 0)
+{
+    Console.Write(number + " is an even number!");
+}
+if (number == 13)
+{
+    Console.Write(number + " is an odd number! And it is Devil's number arrr!!!");
+}
+else
+{
+    Console.Write(number + " is an odd number!");
+}
