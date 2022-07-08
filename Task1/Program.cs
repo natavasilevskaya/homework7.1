d67bb593db8c9e70884018f40990f9d00bf03a2b
Nata.Vasilevskaya <_nata.vasilevskaya11@yandex.ru>
2022-07-08 19:38:23 +0300

    Task1 file added

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// программа на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int n1, n2;
+
+Console.WriteLine("Imput number1: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Imput number2: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+int min = n2;
+
+if (n2>max) 
+{
+    max = n2;
+    min = n1;
+}
+
+Console.WriteLine("Max number is " + max);
+Console.WriteLine("Min number is " + min);
